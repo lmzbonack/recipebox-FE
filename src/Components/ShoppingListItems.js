de983fcf47c68e303ebbfaddabdcd54fc39b0c04
Luc Zbonack <lmzbonack@gmail.com>
2020-03-10 00:07:48 -0700

    Shopping List Service and page in place

--- src/Components/ShoppingListItems.js
+++ src/Components/ShoppingListItems.js
@@ -1,15 +1,121 @@
 import React from 'react'
 
-import { ListGroup, ListGroupItem } from "shards-react";
+import { ListGroup, ListGroupItem, Container } from "shards-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+import { Button, ButtonGroup, Modal, ModalBody, ModalHeader, Form, FormInput, FormGroup  } from 'shards-react'
 
-export default function ShoppingListItems(props) {
-  return (
-    <ListGroup>
-      { props.ingredients.map( (ingredient, index) => (
-        <ListGroupItem className="mt-1 mb-1" key={index}>
-          { ingredient }
-        </ListGroupItem>
-      ))}
-    </ListGroup>
-  )
+import ConfirmDelete from './ConfirmDelete'
+
+export default class ShoppingListItems extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false,
+      activeIndex: '',
+      activeValue: '',
+    };
+    this.toggleModal = this.toggleModal.bind(this)
+    this.openModal = this.openModal.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
+    this.handleUpdate = this.handleUpdate.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.loadDeleteIndex = this.loadDeleteIndex.bind(this)
+  }
+
+  componentDidMount() {
+    this.props.closeModal(this.toggleModal)
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  toggleModal() {
+    this.setState({
+      open: !this.state.open,
+      activeIndex: '',
+      activeValue: ''
+    });
+  }
+
+  openModal(index, value){
+    this.setState({
+      open: !this.state.open,
+      activeIndex: index,
+      activeValue: value
+    });
+  }
+
+  loadDeleteIndex(index) {
+    this.setState({
+      activeIndex: index
+    })
+  }
+
+  handleDelete() {
+    const index = this.state.activeIndex
+    this.props.onIngredientDelete(index)
+    this.closeConfirmModal()
+  }
+
+  handleUpdate() {
+    const payload = {
+      index: this.state.activeIndex,
+      newItem: this.state.activeValue
+    }
+    this.props.onIngredientUpdate(payload)
+  }
+
+  render() {
+    const { open } = this.state
+    return (
+      <Container>
+        <ListGroup>
+          { this.props.ingredients.map( (ingredient, index) => (
+            <ListGroupItem className="mt-1 mb-1" key={index}>
+              { ingredient }
+              <ButtonGroup className='ml-2 float-right'>
+                <Button theme='secondary' onClick={ () => { this.openModal(index, ingredient) } }>
+                  <FontAwesomeIcon className='ml-1' icon={faPencilAlt} />
+                </Button>
+                <Button theme='danger' className='ml-1' onClick={ () => { this.openConfirmModal(); this.loadDeleteIndex(index); } }>
+                  <FontAwesomeIcon className='ml-1' icon={faTimes} />
+                </Button>
+              </ButtonGroup>
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+        <ConfirmDelete title='Delete Ingredient?'
+                       closeModal={close => this.closeConfirmModal = close}
+                       openModal={open => this.openConfirmModal = open}
+                       onDeleteConfirmation={this.handleDelete}/>
+
+        <Modal open={open} toggle={this.toggleModal}>
+            <ModalHeader>Edit Ingredient</ModalHeader>
+          <ModalBody>
+            <Form>
+              <FormGroup>
+                <label htmlFor="#activeValue">Ingredient</label>
+                <FormInput name="activeValue"  value={this.state.activeValue} onChange={this.handleInputChange}/>
+              </FormGroup>
+              <ButtonGroup className='float-right'>
+                <Button className='w-20' theme='secondary' onClick={ () => { this.handleUpdate() } }>
+                  <FontAwesomeIcon className='ml-1' icon={faPencilAlt} />
+                </Button>
+                <Button theme='danger' className='ml-1' onClick={ () => { this.toggleModal() } }>
+                  <FontAwesomeIcon className='ml-1' icon={faTimes} />
+                </Button>
+              </ButtonGroup>
+            </Form>
+          </ModalBody>
+        </Modal>
+      </Container>
+    )
+  }
 }
--- src/Components/SingleShoppingList.js
+++ src/Components/SingleShoppingList.js
@@ -1,29 +1,207 @@
 // Component to render list of items in a single cart
 import React from 'react'
-import { Container, Button, Collapse } from 'shards-react'
+import {
+  Container,
+  Button,
+  Collapse,
+  InputGroup,
+  FormInput,
+  FormGroup} from 'shards-react'
+import { faArrowDown, faArrowUp, faTimes, faPencilAlt, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import ConfirmDelete from './ConfirmDelete'
 import ShoppingListItems from './ShoppingListItems'
+
+import ShoppingListService from '../store/services/ShoppingListService'
+
 
 export default class SingleShoppingList extends React.Component {
   constructor(props) {
     super(props)
-    this.toggle = this.toggle.bind(this)
     this.state = {
-      collapse: false
+      collapse: false,
+      name: '',
+      newIngredient: '',
+      error: []
     }
+    this.toggle = this.toggle.bind(this)
+    this.handleIngredientDelete = this.handleIngredientDelete.bind(this)
+    this.handleIngredientUpdate = this.handleIngredientUpdate.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.updateName = this.updateName.bind(this)
+    this.deleteShoppingList = this.deleteShoppingList.bind(this)
+    this.addIngredient = this.addIngredient.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState(
+      {
+        collapse: this.state.collapse,
+        name: this.props.name,
+        newIngredient: ''
+      }
+    )
   }
 
   toggle() {
     this.setState({ collapse: !this.state.collapse })
   }
 
-  render () {
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  async addIngredient() {
+    let newIngredients = this.props.ingredients
+    if (this.state.newIngredient.length === 0) {
+      this.setState({
+        error: 'Cannot add blank ingredient'
+      })
+      return
+    }
+    newIngredients.unshift(this.state.newIngredient)
+    const requestPayload = {
+      name: this.props.name,
+      ingredients: newIngredients
+    }
+    try {
+      let addIngredientResponse = await ShoppingListService.update(this.props.id, requestPayload)
+      if (addIngredientResponse.status === 200) {
+        const payload = {
+          id: this.props.id
+        }
+        this.props.onIngredientChangeTop(payload)
+        this.setState({
+          newIngredient: ''
+        })
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async updateName(name) {
+    const requestPayload = {
+      name: name,
+      ingredients: this.props.ingredients
+    }
+    try {
+      let updateNameResponse = await ShoppingListService.update(this.props.id, requestPayload)
+      if (updateNameResponse.status === 200) {
+        const payload = {
+          id: this.props.id
+        }
+        this.props.onIngredientChangeTop(payload)
+      }
+    } catch (error) {
+      // Handle that error elegantly and pop it into state
+      console.error(error)
+    }
+  }
+
+  async deleteShoppingList() {
+    try {
+      let deleteRecipeResponse = await ShoppingListService.delete(this.props.id)
+      if (deleteRecipeResponse.status === 204) {
+        const payload = {
+          id: this.props.id
+        }
+        this.props.onIngredientChangeTop(payload)
+      }
+    } catch (error) {
+      // Handle that error elegantly and pop it into state
+      console.error(error)
+    }
+  }
+
+  async handleIngredientDelete(index) {
+    // Make changes using API
+    let changedIngredients = this.props.ingredients
+    changedIngredients.splice(index, 1)
+
+    const requestPayload = {
+      name: this.props.name,
+      ingredients: changedIngredients
+    }
+    try {
+      let deleteIngredientResponse = await ShoppingListService.update(this.props.id, requestPayload)
+      if (deleteIngredientResponse.status === 200) {
+        const payload = {
+          index: index,
+          id: this.props.id
+        }
+        this.props.onIngredientChangeTop(payload)
+      }
+    } catch (error) {
+      // Handle that error elegantly and pop it into state
+      console.error(error)
+    }
+  }
+
+  async handleIngredientUpdate(payload) {
+    let changedIngredients = this.props.ingredients
+    changedIngredients[payload.index] = payload.newItem
+
+    const requestPayload = {
+      name: this.props.name,
+      ingredients: changedIngredients
+    }
+    try {
+      let updateIngredientResponse = await ShoppingListService.update(this.props.id, requestPayload)
+      if (updateIngredientResponse.status === 200) {
+        this.closeChildModal()
+        const parentsPayload = {
+          id: this.props.id
+        }
+        this.props.onIngredientChangeTop(parentsPayload)
+      }
+    } catch (error) {
+      // Handle that error elegantly and pop it into state
+      console.error(error)
+    }
+  }
+
+  render() {
     return (
       <Container>
-        <h4>{ this.props.name }</h4>
-        <Button onClick={this.toggle}>Expand</Button>
+        <ConfirmDelete title='Delete List?'
+                       closeModal={close => this.closeConfirmModal = close}
+                       openModal={open => this.openConfirmModal = open}
+                       onDeleteConfirmation={this.deleteShoppingList}/>
+        <Button className='mb-2' outline onClick={this.toggle}>{ this.props.name }
+          <FontAwesomeIcon className='ml-1' icon = { this.state.collapse ? faArrowUp : faArrowDown } />
+        </Button>
+        { this.state.collapse &&
+          <FormGroup>
+            <InputGroup className='w-50 mb-2'>
+              <FormInput name='name' value={this.state.name} onChange={this.handleInputChange}/>
+              <Button className='ml-1' theme="secondary" onClick={ () => { this.updateName(this.state.name) } }>
+                <FontAwesomeIcon className='ml-1' icon={faPencilAlt} />
+              </Button>
+              <Button className='ml-1' theme="danger" onClick={ () => { this.openConfirmModal() } }>
+                <FontAwesomeIcon className='ml-1' icon={faTimes} />
+              </Button>
+            </InputGroup>
+            <InputGroup className='w-50'>
+              <FormInput placeholder= 'add ingredient' name='newIngredient' value={this.state.newIngredient} onChange={this.handleInputChange}/>
+              <Button className='ml-1' theme="success" onClick={ () => { this.addIngredient() } }>
+                <FontAwesomeIcon className='ml-1' icon={faPlus} />
+              </Button>
+            </InputGroup>
+            <p>{ this.state.error }</p>
+          </FormGroup>
+        }
         <Collapse open={ this.state.collapse }>
-          <ShoppingListItems ingredients={this.props.ingredients}/>
+          <ShoppingListItems ingredients={this.props.ingredients}
+                             onIngredientDelete={this.handleIngredientDelete}
+                             onIngredientUpdate={this.handleIngredientUpdate}
+                             closeModal={close => this.closeChildModal = close}/>
         </Collapse>
       </Container>
     )
