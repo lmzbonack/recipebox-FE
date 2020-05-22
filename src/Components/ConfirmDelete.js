bf8b97986681de5cccd0242addf79a41b0496265
Luc Zbonack <lmzbonack@gmail.com>
2020-05-22 12:31:46 -0700

    Small modal style change + login and logout button work properly

--- src/Components/ConfirmDelete.js
+++ src/Components/ConfirmDelete.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Button, ButtonGroup, Modal, ModalBody, ModalHeader, Container  } from 'shards-react'
+import { Button, ButtonGroup, Modal, ModalBody, ModalHeader } from 'shards-react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes, faCheck } from "@fortawesome/free-solid-svg-icons";
 
@@ -35,20 +35,18 @@ export default class ConfirmDelete extends React.Component {
   render() {
     const { open } = this.state
     return (
-      <Modal className='w-25' open={open} toggle={this.toggleModal}>
-        <Container className='mb-3'>
-          <ModalHeader>{this.props.title}</ModalHeader>
-          <ModalBody>
-            <ButtonGroup className='float-left'>
-              <Button className='w-20' theme='success' onClick={ () => { this.confirmDelete() } }>
-                <FontAwesomeIcon className='ml-1' icon={faCheck} />
-              </Button>
-              <Button theme='danger' className='ml-1' onClick={ () => { this.toggleModal() } }>
-                <FontAwesomeIcon className='ml-1' icon={faTimes} />
-              </Button>
-            </ButtonGroup>
-          </ModalBody>
-        </Container>
+      <Modal size="sm" open={open} toggle={this.toggleModal}>
+        <ModalHeader>{this.props.title}</ModalHeader>
+        <ModalBody>
+          <ButtonGroup className='float-left'>
+            <Button className='w-20' theme='success' onClick={ () => { this.confirmDelete() } }>
+              <FontAwesomeIcon className='ml-1' icon={faCheck} />
+            </Button>
+            <Button theme='danger' className='ml-1' onClick={ () => { this.toggleModal() } }>
+              <FontAwesomeIcon className='ml-1' icon={faTimes} />
+            </Button>
+          </ButtonGroup>
+        </ModalBody>
       </Modal>
     )
   }
